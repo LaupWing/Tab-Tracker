f990ddcb9453f14486a8822271228da664d4292e
LaupWing <loc_strict@hotmail.com>
2019-05-28 16:28:32 +0200

    Dont get it...

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
-const express    = require('express')
-const bodyParser = require('body-parser')
-const cors       = require('cors')
-const morgan     = require('morgan')
-const port       = 8081
+const express     = require('express')
+const bodyParser  = require('body-parser')
+const cors        = require('cors')
+const morgan      = require('morgan')
+const port        = 8081
+const {sequelize} = require('./models')
+const config      = require('./config/config')
 
 const app = express()
 // Cors is for security reasons so that other clients can connect to the server safely
@@ -12,9 +14,13 @@ app
     .use(bodyParser.json())
     .use(cors())
     // Post method will be handled in the client in the folder services > Auth and API
-    .post('/register', (req,res)=>{
-        res.send({
-            message: "Your user was registered! Have fun!"+ req.body.email
-        })
+
+require('./routes')(app)
+    
+    // Sequelize.sync() connect to any database that is connected to sequelize
+    // Create the tables that doesnt exist
+sequelize.sync()
+    .then(()=>{
+        app.listen(config.port)
+        console.log(`Server is listenen on port${config.port}`)
     })
-    .listen(process.env.PORT||port)
